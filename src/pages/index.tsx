7bcd3333c1c9cd764a58c329b9d74071fd1bf7fd
mtguerson <mtguerson@gmail.com>
2024-08-03 23:56:06 -0400

    feat: add carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Image from "next/image"
 import { HomeContainer, Product } from "../styles/pages/home"
 
+import { useKeenSlider } from 'keen-slider/react'
+
 import camiseta1 from '../assets/camisetas/1.png'
 import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Page() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
         <footer>
@@ -17,8 +28,17 @@ export default function Page() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 89,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta Y</strong>
