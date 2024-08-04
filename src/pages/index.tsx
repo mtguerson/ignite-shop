7b47ff40d29eeda9bcb588df26d48b956c3c7c76
mtguerson <mtguerson@gmail.com>
2024-08-03 23:35:00 -0400

    feat: home ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
-import { styled } from "../styles"
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-  borderRadius: '4',
-  border: 0,
-  padding: '4px px'
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Page() {
-  return <Button>Enviar</Button>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 89,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
